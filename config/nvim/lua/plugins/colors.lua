c09d626a3c62468925321e050cd4f52214526111
Danil Antoshin <antoshindanil@ya.ru>
2024-08-16 01:23:07 +0200

    dark contrast

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -14,7 +14,7 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.opt.background = "dark"
-        vim.cmd("colorscheme github_dark_default")
+        vim.cmd("colorscheme github_dark_high_contrast")
       end,
       set_light_mode = function()
         vim.opt.background = "light"
