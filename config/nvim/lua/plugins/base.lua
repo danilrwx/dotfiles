8165fab54f88bfb50617921eeacdbc7bf8ed9d05
Danil Antoshin <antoshindanil@ya.ru>
2024-10-13 00:09:57 +0200

    feat: another try for lazyvim

--- config/nvim/lua/plugins/base.lua
+++ config/nvim/lua/plugins/base.lua
@@ -1,90 +1,125 @@
 return {
   {
-    "mbbill/undotree",
-    keys = { { "<Leader>u", vim.cmd.UndotreeToggle } },
-  },
-  {
-    "echasnovski/mini.nvim",
-    version = false,
-    config = function()
-      require("mini.bracketed").setup()
-      require("mini.diff").setup({ view = { style = 'sign' } })
-      require("mini.surround").setup()
-      require("mini.trailspace").setup()
-    end
-  },
-  {
-    "tpope/vim-fugitive",
-    dependencies = { "shumphrey/fugitive-gitlab.vim", "tpope/vim-rhubarb" },
-    config = function()
-      vim.keymap.set("n", "<Leader>gs", ":tab Git<CR>", { desc = "Open Fugitive" })
-      vim.keymap.set("n", "<Leader>gl", ":tab Git log --follow -p %<CR>", { desc = "Open Git log" })
-      vim.keymap.set("n", "<Leader>gL", ":tab Git log<CR>", { desc = "Open Git log" })
-      vim.keymap.set("n", "<Leader>gK", ":tab Git log -p<CR>", { desc = "Open Git log" })
-      vim.keymap.set("n", "<Leader>gb", ":tab Git blame<CR>", { desc = "Open Git blame" })
-      vim.keymap.set("n", "<Leader>gB", ":GBrowse<CR>", { desc = "Open Git browse" })
-      vim.keymap.set("n", "<Leader>gd", ":tab Git diff<CR>", { desc = "Open Git diff" })
-      vim.keymap.set("n", "<Leader>gP", ":Git push<CR>", { desc = "Git push" })
-      vim.keymap.set("n", "<Leader>gp", ":Git pull --rebase<CR>", { desc = "Git pull with rebase" })
-
-      vim.g.fugitive_gitlab_domains = { "https://rscz.ru" }
-    end
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require "telescope".setup {
-        defaults = { layout_config = { horizontal = { width = 0.95 } } },
-        pickers = { colorscheme = { enable_preview = true } },
-      }
-
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<Leader>f", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<Leader>/", builtin.live_grep, { desc = "Grep files" })
-      vim.keymap.set("x", "<Leader>/", builtin.grep_string, { desc = "Grep visual string" })
-      vim.keymap.set("n", "<Leader>b", builtin.buffers, { desc = "Find buffers" })
-      vim.keymap.set("n", "<Leader>r", builtin.registers, { desc = "Find registers" })
-    end
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "RRethy/nvim-treesitter-endwise",
-      "Wansmer/treesj",
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "torte",
     },
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "vimdoc", "ruby", "go" },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-        endwise = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = '<C-Space>',
-            node_incremental = '<C-Space>',
-            node_decremental = '<C-s>',
-          },
-        },
-      })
-
-      require("treesj").setup()
-      vim.keymap.set("n", "<Leader>m", require("treesj").toggle, { desc = "toggle split treesitter object" })
-    end
   },
+  {
+    "catppuccin/nvim",
+    enabled = false,
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+  {
+    "folke/persistence.nvim",
+    enabled = false,
+  },
+  {
+    "stevearc/dressing.nvim",
+    enabled = false,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    enabled = false,
+  },
+  {
+    "MagicDuck/grug-far.nvim",
+    enabled = false,
+  },
+  {
+    "rcarriga/nvim-notify",
+    enabled = false,
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    enabled = false,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      document_highlight = {
+        enabled = false,
+      },
+      codelens = {
+        enabled = false,
+      },
+      inlay_hints = {
+        enabled = false,
+      },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "LazyFile",
+    opts = {
+      signcolumn = true,
+      numhl = true,
+      current_line_blame = true,
+    },
+  },
+
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader>gc", false },
+      { "<leader>gs", false },
+    },
+    opts = {
+      winopts = {
+        fullscreen = true,
+      },
+    },
+  },
+
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("lsp_signature").setup(opts)
+    end,
+  },
+
   {
     "chrisgrieser/nvim-various-textobjs",
     lazy = false,
     opts = { useDefaultKeymaps = true },
   },
+
   {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-    }
+    "tpope/vim-fugitive",
+    lazy = false,
+    dependencies = { "shumphrey/fugitive-gitlab.vim", "tpope/vim-rhubarb" },
+    config = function()
+      vim.g.fugitive_gitlab_domains = { "https://rscz.ru" }
+    end,
   },
-  { "slim-template/vim-slim", },
+
+  { import = "lazyvim.plugins.extras.editor.fzf" },
+  { import = "lazyvim.plugins.extras.dap.core" },
+  { import = "lazyvim.plugins.extras.editor.refactoring" },
+  { import = "lazyvim.plugins.extras.test.core" },
+  { import = "lazyvim.plugins.extras.dap.core" },
+  { import = "lazyvim.plugins.extras.coding.neogen" },
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.ruby" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
 }
