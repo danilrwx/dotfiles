99d31e2d76699f0d895c5ba5caf2677963cbb09d
Daniil Antoshin <daniil.antoshin@flant.com>
2025-03-27 12:53:04 +0200

    +

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,39 +1,69 @@
-local langmap_keys     = {
-  'ёЁ;`~', '№;#',
-  'йЙ;qQ', 'цЦ;wW', 'уУ;eE', 'кК;rR', 'еЕ;tT', 'нН;yY', 'гГ;uU', 'шШ;iI', 'щЩ;oO', 'зЗ;pP', 'хХ;[{', 'ъЪ;]}',
-  'фФ;aA', 'ыЫ;sS', 'вВ;dD', 'аА;fF', 'пП;gG', 'рР;hH', 'оО;jJ', 'лЛ;kK', 'дД;lL', [[жЖ;\;:]], [[эЭ;'\"]],
-  'яЯ;zZ', 'чЧ;xX', 'сС;cC', 'мМ;vV', 'иИ;bB', 'тТ;nN', 'ьЬ;mM', [[бБ;\,<]], 'юЮ;.>',
+local langmap_keys = {
+  "ёЁ;`~",
+  "№;#",
+  "йЙ;qQ",
+  "цЦ;wW",
+  "уУ;eE",
+  "кК;rR",
+  "еЕ;tT",
+  "нН;yY",
+  "гГ;uU",
+  "шШ;iI",
+  "щЩ;oO",
+  "зЗ;pP",
+  "хХ;[{",
+  "ъЪ;]}",
+  "фФ;aA",
+  "ыЫ;sS",
+  "вВ;dD",
+  "аА;fF",
+  "пП;gG",
+  "рР;hH",
+  "оО;jJ",
+  "лЛ;kK",
+  "дД;lL",
+  [[жЖ;\;:]],
+  [[эЭ;'\"]],
+  "яЯ;zZ",
+  "чЧ;xX",
+  "сС;cC",
+  "мМ;vV",
+  "иИ;bB",
+  "тТ;nN",
+  "ьЬ;mM",
+  [[бБ;\,<]],
+  "юЮ;.>",
 }
 
-vim.o.langmap          = table.concat(langmap_keys, ',')
-vim.g.mapleader        = " "
-vim.opt.undodir        = os.getenv("HOME") .. "/.vim/nfiles/undo"
-vim.opt.undofile       = true
-vim.g.maplocalLeader   = "\\"
-vim.opt.number         = false
-vim.opt.numberwidth    = 2
+vim.o.langmap = table.concat(langmap_keys, ",")
+vim.g.mapleader = " "
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/nfiles/undo"
+vim.opt.undofile = true
+vim.g.maplocalLeader = "\\"
+vim.opt.number = false
+vim.opt.numberwidth = 2
 vim.opt.relativenumber = true
-vim.opt.expandtab      = true
-vim.opt.smarttab       = true
-vim.opt.smartindent    = true
-vim.opt.softtabstop    = 2
-vim.opt.tabstop        = 2
-vim.opt.shiftwidth     = 2
-vim.opt.signcolumn     = "yes"
-vim.opt.laststatus     = 1
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.signcolumn = "yes"
+vim.opt.laststatus = 1
 
-if vim.fn.executable('ugrep') == 1 then
+if vim.fn.executable("ugrep") == 1 then
   vim.opt.grepprg = "ugrep -RInk --tabs=1 --ignore-files --exclude='zz_generated*' --exclude-dir='generated'"
 else
   vim.opt.grepprg = "grep -RIn --exclude='zz_generated*' --exclude-dir='generated'"
 end
 
-vim.fn.sign_define('DiagnosticSignError', { text = '🔥', texthl = 'DiagnosticSignError' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '💫', texthl = 'DiagnosticSignWarn' })
-vim.fn.sign_define('DiagnosticSignInfo', { text = '🔩', texthl = 'DiagnosticSignInfo' })
-vim.fn.sign_define('DiagnosticSignHint', { text = '📎', texthl = 'DiagnosticSignHint' })
+vim.fn.sign_define("DiagnosticSignError", { text = "🚨", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "💫", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "🔩", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "📎", texthl = "DiagnosticSignHint" })
 
-vim.diagnostic.config({ virtual_text = { prefix = '🐗' } })
+vim.diagnostic.config({ virtual_text = { prefix = "🐗" } })
 
 vim.filetype.add({
   filename = {
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -4,71 +4,78 @@ return {
     cmd = { "LspInfo", "LspInstall", "LspStart" },
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-      -- require('lspconfig').gopls.setup({
-      --   root_dir = function(fname)
-      --     local util = require 'lspconfig.util'
-      --     local async = require 'lspconfig.async'
-      --     local mod_cache = nil
-      --
-      --     -- see: https://github.com/neovim/nvim-lspconfig/issues/804
-      --     if not mod_cache then
-      --       local result = async.run_command { 'go', 'env', 'GOMODCACHE' }
-      --       if result and result[1] then
-      --         mod_cache = vim.trim(result[1])
-      --       else
-      --         mod_cache = vim.fn.system 'go env GOMODCACHE'
-      --       end
-      --     end
-      --     if mod_cache and fname:sub(1, #mod_cache) == mod_cache then
-      --       local clients = util.get_lsp_clients { name = 'gopls' }
-      --       if #clients > 0 then
-      --         return clients[#clients].config.root_dir
-      --       end
-      --     end
-      --     return util.root_pattern('go.work', '.git', 'go.mod')(fname)
-      --   end,
-      -- })
+      require("lspconfig").gopls.setup({
+        root_dir = function(fname)
+          local util = require("lspconfig.util")
+          local async = require("lspconfig.async")
+          local mod_cache = nil
 
-      require('lspconfig').golangci_lint_ls.setup({
-        filetypes = { 'go', 'gomod' },
-        init_options = { command = { "golangci-lint", "run", "--out-format", "json", "--issues-exit-code=1" } }
+          -- see: https://github.com/neovim/nvim-lspconfig/issues/804
+          if not mod_cache then
+            local result = async.run_command({ "go", "env", "GOMODCACHE" })
+            if result and result[1] then
+              mod_cache = vim.trim(result[1])
+            else
+              mod_cache = vim.fn.system("go env GOMODCACHE")
+            end
+          end
+          if mod_cache and fname:sub(1, #mod_cache) == mod_cache then
+            local clients = util.get_lsp_clients({ name = "gopls" })
+            if #clients > 0 then
+              return clients[#clients].config.root_dir
+            end
+          end
+          return util.root_pattern("go.work", ".git", "go.mod")(fname)
+        end,
       })
 
-      require('lspconfig').lua_ls.setup({
+      require("lspconfig").golangci_lint_ls.setup({
+        filetypes = { "go", "gomod" },
+        init_options = { command = { "golangci-lint", "run", "--out-format", "json", "--issues-exit-code=1" } },
+      })
+
+      require("lspconfig").lua_ls.setup({
         on_init = function(client)
           if client.workspace_folders then
             local path = client.workspace_folders[1].name
-            if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
+            if
+              path ~= vim.fn.stdpath("config")
+              and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
+            then
               return
             end
           end
 
-          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-            runtime = { version = 'LuaJIT' },
+          client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+            runtime = { version = "LuaJIT" },
             workspace = { checkThirdParty = false, library = { vim.env.VIMRUNTIME } },
           })
         end,
-        settings = { Lua = {} }
+        settings = { Lua = {} },
       })
-    end
+    end,
   },
 
-  -- {
-  --   "stevearc/conform.nvim",
-  --   opts = {
-  --     formatters_by_ft = {
-  --       lua = { "stylua" },
-  --       go = { "golangci-lint", "gofmt" },
-  --       ["*"] = { "codespell" },
-  --       ["_"] = { "trim_whitespace" },
-  --     },
-  --     format_on_save = {
-  --       -- I recommend these options. See :help conform.format for details.
-  --       lsp_format = "fallback",
-  --       timeout_ms = 500,
-  --     },
-  --   },
-  -- },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- go = { "golangci", "gofmt" },
+        go = { "goimports", "gofumpt" },
+        ["*"] = { "codespell" },
+        ["_"] = { "trim_whitespace" },
+      },
+      format_on_save = { lsp_format = "fallback", timeout_ms = 500 },
+      formatters = {
+        golangci = {
+          command = "golangci-lint",
+          args = { "run", "--out-format", "json", "--fix", "$FILENAME" },
+          -- cwd = require("conform.util").root_file({ ".golangci.yaml" }),
+        },
+      },
+    },
+  },
 
   {
     "Wansmer/symbol-usage.nvim",
