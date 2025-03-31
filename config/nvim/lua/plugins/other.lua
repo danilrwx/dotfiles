b88554c40398d76e4dc65956e41025ad9d573a76
Daniil Antoshin <daniil.antoshin@flant.com>
2025-03-31 10:50:59 +0200

    +
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -3,13 +3,4 @@ return {
     "mbbill/undotree",
     keys = { { "<leader>u", "<cmd>UndotreeToggle<cr>" } },
   },
-  {
-    'stevearc/oil.nvim',
-    opts = {},
-    dependencies = {
-      -- "nvim-tree/nvim-web-devicons",
-    },
-    keys = { { "-", "<cmd>Oil<cr>" } },
-    lazy = false,
-  },
 }
