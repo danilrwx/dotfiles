12b544e11a458a0ead79872d066d1495835f33b9
Daniil Antoshin <daniil.antoshin@flant.com>
2026-01-19 11:01:05 +0200

    upd
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -14,45 +14,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local langmap_keys = {
-  "ёЁ;`~",
-  "№;#",
-  "йЙ;qQ",
-  "цЦ;wW",
-  "уУ;eE",
-  "кК;rR",
-  "еЕ;tT",
-  "нН;yY",
-  "гГ;uU",
-  "шШ;iI",
-  "щЩ;oO",
-  "зЗ;pP",
-  "хХ;[{",
-  "ъЪ;]}",
-  "фФ;aA",
-  "ыЫ;sS",
-  "вВ;dD",
-  "аА;fF",
-  "пП;gG",
-  "рР;hH",
-  "оО;jJ",
-  "лЛ;kK",
-  "дД;lL",
-  [[жЖ;\;:]],
-  [[эЭ;'\"]],
-  "яЯ;zZ",
-  "чЧ;xX",
-  "сС;cC",
-  "мМ;vV",
-  "иИ;bB",
-  "тТ;nN",
-  "ьЬ;mM",
-  [[бБ;\,<]],
-  "юЮ;.>",
-}
-vim.o.langmap = table.concat(langmap_keys, ",")
-
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
@@ -265,7 +226,7 @@ require("lazy").setup({
   checker = { enabled = false },
 })
 
-vim.opt.termguicolors = false
+vim.opt.termguicolors = true
 vim.cmd.colorscheme("torte")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = nil })
