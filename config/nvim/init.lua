76161092c5d51e379c6ab0a6a286fcca9903d9d6
Daniil Antoshin <antoshindanil@ya.ru>
2026-03-09 16:59:16 +0200

    update
    Signed-off-by: Daniil Antoshin <antoshindanil@ya.ru>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -14,6 +14,45 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local langmap_keys = {
+  "ёЁ;`~",
+  "№;#",
+  "йЙ;qQ",
+  "цЦ;wW",
+  "уУ;eE",
+  "кК;rR",
+  "еЕ;tT",
+  "нН;yY",
+  "гГ;uU",
+  "шШ;iI",
+  "щЩ;oO",
+  "зЗ;pP",
+  "хХ;[{",
+  "ъЪ;]}",
+  "фФ;aA",
+  "ыЫ;sS",
+  "вВ;dD",
+  "аА;fF",
+  "пП;gG",
+  "рР;hH",
+  "оО;jJ",
+  "лЛ;kK",
+  "дД;lL",
+  [[жЖ;\;:]],
+  [[эЭ;'\"]],
+  "яЯ;zZ",
+  "чЧ;xX",
+  "сС;cC",
+  "мМ;vV",
+  "иИ;bB",
+  "тТ;nN",
+  "ьЬ;mM",
+  [[бБ;\,<]],
+  "юЮ;.>",
+}
+vim.o.langmap = table.concat(langmap_keys, ",")
+
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
