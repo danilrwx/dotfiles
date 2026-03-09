ec678d6d8683ab94f55ff150a3df5a793e1d5f9c
Daniil Antoshin <antoshindanil@ya.ru>
2026-03-09 16:54:14 +0200

    +

--- config/nvim/lua/plugins/go.lua
+++ config/nvim/lua/plugins/go.lua
@@ -1,9 +1,16 @@
 return {
   {
     "kyoh86/vim-go-coverage",
+    lazy = false,
+    keys = {
+      { "<leader>tc", "<cmd>GoCover<cr>" },
+      { "<leader>tC", "<cmd>GoCoverClear<cr>" },
+    },
   },
+
   {
     "vim-test/vim-test",
+    lazy = false,
     keys = {
       { "<leader>tr", "<cmd>TestNearest<cr>" },
       { "<leader>tt", "<cmd>TestFile<cr>" },
