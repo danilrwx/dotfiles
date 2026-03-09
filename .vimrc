e628136ae477953a9a92e9e88731b6f3e2596508
Daniil Antoshin <antoshindanil@ya.ru>
2026-03-09 16:56:55 +0200

    update config

--- .vimrc
+++ .vimrc
@@ -1,27 +1,27 @@
 if has("eval")             " vim-tiny lacks 'eval'
-	let skip_defaults_vim = 1
+  let skip_defaults_vim = 1
 endif
 
 if filereadable(expand("~/.vim/autoload/plug.vim"))
-	call plug#begin('~/.local/share/vim/plugins')
-	Plug 'markonm/traces.vim'
-	Plug 'sheerun/vim-polyglot'
+  call plug#begin('~/.local/share/vim/plugins')
+  Plug 'markonm/traces.vim'
+  Plug 'sheerun/vim-polyglot'
 
-	Plug 'tpope/vim-fugitive'
-	Plug 'airblade/vim-gitgutter'
+  " Plug 'tpope/vim-fugitive'
+  Plug 'airblade/vim-gitgutter'
 
-	Plug 'yegappan/lsp'
+  Plug 'yegappan/lsp'
 
-	Plug 'christoomey/vim-tmux-navigator'
-	Plug 'preservim/vimux'
-	Plug 'vim-test/vim-test'
-	Plug 'sebdah/vim-delve'
-	Plug 'charlespascoe/vim-go-syntax'
+  Plug 'christoomey/vim-tmux-navigator'
+  Plug 'preservim/vimux'
+  Plug 'vim-test/vim-test'
+  Plug 'sebdah/vim-delve'
+  Plug 'charlespascoe/vim-go-syntax'
 
-	Plug 'habamax/vim-dir'
+  Plug 'habamax/vim-dir'
 
-	Plug 'laktak/tome'
-	call plug#end()
+  Plug 'laktak/tome'
+  call plug#end()
 endif
 
 set nocompatible
@@ -64,15 +64,15 @@ set mouse=a                " Enable mouse support
 " The fish shell is not very compatible to other shells and unexpectedly
 " breaks things that use 'shell'.
 if &shell =~# 'fish$'
-	set shell=/bin/bash
+  set shell=/bin/bash
 endif
 
 " Made grep great again
 if executable('ugrep')
-	set grepprg=ugrep\ -RInk\ --tabs=1\ --ignore-files\ --exclude='zz_generated*'\ --exclude-dir='generated'
-	set grepformat=%f:%l:%c:%m,%f+%l+%c+%m,%-G%f\\\|%l\\\|%c\\\|%m
+  set grepprg=ugrep\ -RInk\ --tabs=1\ --ignore-files\ --exclude='zz_generated*'\ --exclude-dir='generated'
+  set grepformat=%f:%l:%c:%m,%f+%l+%c+%m,%-G%f\\\|%l\\\|%c\\\|%m
 else
-	set grepprg=grep\ --exclude='*zz_generated*'\ --exclude-dir='generated'
+  set grepprg=grep\ --exclude='*zz_generated*'\ --exclude-dir='generated'
 endif
 
 " Put all temporary files under the same directory.
@@ -104,32 +104,32 @@ highlight DiffChange ctermfg=16
 highlight DiffDelete ctermfg=16
 
 if has("gui_macvim")
-	set guifont=Iosevka\ Nerd\ Font:h20
-	let macvim_hig_shift_movement = 1
+  set guifont=Iosevka\ Nerd\ Font:h20
+  let macvim_hig_shift_movement = 1
 endif
 
 if has('patch-8.1.0311')
-	packadd! cfilter
+  packadd! cfilter
 endif
 
 if has('patch-9.1.0375')
-	packadd! comment
+  packadd! comment
 endif
 
 if has('patch-9.1.1230')
-	packadd! hlyank
+  packadd! hlyank
 endif
 
 if has('patch-9.0.1799')
-	packadd! editorconfig
+  packadd! editorconfig
 endif
 
 let mapleader=" "
 
 if executable('ugrep')
-	nnoremap <leader>f :call FzyCommand("ugrep '' . -Rl -I --ignore-files --exclude='zz_generated*' --exclude-dir='generated'", ":e")<cr>
+  nnoremap <leader>f :call FzyCommand("ugrep '' . -Rl -I --ignore-files --exclude='zz_generated*' --exclude-dir='generated'", ":e")<cr>
 else
-	nnoremap <leader>f :call FzyCommand("find . -type f", ":e")<cr>
+  nnoremap <leader>f :call FzyCommand("find . -type f", ":e")<cr>
 endif
 
 nnoremap <leader>b :call FzyBuffer()<cr>
@@ -170,98 +170,96 @@ let g:gitgutter_sign_priority = 0
 let g:gitgutter_preview_win_floating = 1
 
 let lspOpts = #{
-			\  autoHighlightDiags: v:true,
-			\  useQuickfixForLocations: v:true,
-			\  semanticHighlight: v:false,
-			\  showInlayHints: v:false,
-			\ }
+      \  autoHighlightDiags: v:true,
+      \  useQuickfixForLocations: v:true,
+      \  semanticHighlight: v:false,
+      \  showInlayHints: v:false,
+      \ }
 
 autocmd User LspSetup call LspOptionsSet(lspOpts)
 
 let lspServers = [
-			\ #{
-			\    name: 'clangd',
-			\    filetype: ['c', 'cpp'],
-			\    path: '/usr/bin/clangd',
-			\    args: ['--background-index']
-			\  },
-			\ #{
-			\    name: 'golang',
-			\    filetype: ['go', 'gomod'],
-			\    path: '/Users/danil/go/bin/gopls',
-			\    args: ['serve'],
-			\    syncInit: v:false,
-			\    workspaceConfig: #{
-			\      gopls: #{
-			\        hints: #{
-			\          assignVariableTypes: v:true,
-			\          compositeLiteralFields: v:true,
-			\          compositeLiteralTypes: v:true,
-			\          constantValues: v:true,
-			\          functionTypeParameters: v:true,
-			\          parameterNames: v:true,
-			\          rangeVariableTypes: v:true,
-			\          semanticTokens: v:false
-			\        }
-			\      }
-			\    }
-			\ }
-			\]
+      \ #{
+      \    name: 'clangd',
+      \    filetype: ['c', 'cpp'],
+      \    path: '/usr/bin/clangd',
+      \    args: ['--background-index']
+      \  },
+      \ #{
+      \    name: 'golang',
+      \    filetype: ['go', 'gomod'],
+      \    path: '/Users/danil/go/bin/gopls',
+      \    args: ['serve'],
+      \    syncInit: v:false,
+      \    workspaceConfig: #{
+      \      gopls: #{
+      \        hints: #{
+      \          assignVariableTypes: v:true,
+      \          compositeLiteralFields: v:true,
+      \          compositeLiteralTypes: v:true,
+      \          constantValues: v:true,
+      \          functionTypeParameters: v:true,
+      \          parameterNames: v:true,
+      \          rangeVariableTypes: v:true,
+      \          semanticTokens: v:false
+      \        }
+      \      }
+      \    }
+      \ }
+      \]
 autocmd User LspSetup call LspAddServer(lspServers)
 
 function! s:on_lsp_buffer_enabled()
-	setlocal omnifunc=g:LspOmniFunc
-	setlocal tagfunc=lsp#lsp#TagFunc
-	setlocal formatexpr=lsp#lsp#FormatExpr()
+  setlocal omnifunc=g:LspOmniFunc
+  setlocal tagfunc=lsp#lsp#TagFunc
+  setlocal formatexpr=lsp#lsp#FormatExpr()
 
-	nnoremap grr :LspShowReferences<cr>
-	nnoremap gri :LspPeekImpl<cr>
-	nnoremap K   :LspHover<cr>
-	nnoremap grs :LspDocumentSymbol<cr>
-	nnoremap grS :LspSymbolSearch<cr>
-	nnoremap gra :LspCodeAction<cr>
-	nnoremap grc :LspCodeLens<cr>
-	nnoremap grn :LspRename<cr>
-	nnoremap grf :LspFormat<cr>
+  nnoremap grr :LspShowReferences<cr>
+  nnoremap gri :LspPeekImpl<cr>
+  nnoremap K   :LspHover<cr>
+  nnoremap grs :LspDocumentSymbol<cr>
+  nnoremap grS :LspSymbolSearch<cr>
+  nnoremap gra :LspCodeAction<cr>
+  nnoremap grc :LspCodeLens<cr>
+  nnoremap grn :LspRename<cr>
+  nnoremap grf :LspFormat<cr>
+  nnoremap <c-w>d :LspDiag current<cr>
+  nnoremap [d :LspDiag prev<cr>
+  nnoremap ]d :LspDiag next<cr>
 
-	nnoremap <c-w>d :LspDiag current<cr>
-	nnoremap [d :LspDiag prev<cr>
-	nnoremap ]d :LspDiag next<cr>
-
-	autocmd! BufWritePre *.go call execute('LspFormat')
+  autocmd! BufWritePre *.go call execute('LspFormat')
 endfunction
 autocmd User LspAttached call s:on_lsp_buffer_enabled()
 
 function! FzyCommand(choice_command, vim_command)
-	try
-		let output = system(a:choice_command . " | fzy ")
-	catch /Vim:Interrupt/
-		" Swallow errors from ^C, allow redraw! below
-	endtry
-	redraw!
-	if v:shell_error == 0 && !empty(output)
-		exec a:vim_command . ' ' . output
-	endif
+  try
+    let output = system(a:choice_command . " | fzy ")
+  catch /Vim:Interrupt/
+    " Swallow errors from ^C, allow redraw! below
+  endtry
+  redraw!
+  if v:shell_error == 0 && !empty(output)
+    exec a:vim_command . ' ' . output
+  endif
 endfunction
 
 function! FzyBuffer()
-	let bufnrs = filter(range(1, bufnr("$")), 'buflisted(v:val)')
-	let buffers = map(bufnrs, 'bufname(v:val)')
-	call FzyCommand('echo "' . join(buffers, "\n") . '"', ":b")
+  let bufnrs = filter(range(1, bufnr("$")), 'buflisted(v:val)')
+  let buffers = map(bufnrs, 'bufname(v:val)')
+  call FzyCommand('echo "' . join(buffers, "\n") . '"', ":b")
 endfunction
 
 function! ToggleQuickFix()
-	if empty(filter(getwininfo(), 'v:val.quickfix'))
-		copen
-	else
-		cclose
-	endif
+  if empty(filter(getwininfo(), 'v:val.quickfix'))
+    copen
+  else
+    cclose
+  endif
 endfunction
 
 function! TrimWhitespace()
-	let l:save = winsaveview()
-	keeppatterns %s/\s\+$//e
-	call winrestview(l:save)
+  let l:save = winsaveview()
+  keeppatterns %s/\s\+$//e
+  call winrestview(l:save)
 endfun
 autocmd BufWritePre * call TrimWhitespace()
-
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -17,14 +17,9 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.cmd("let g:test#strategy = 'vimux'")
-vim.g.VimuxHeight = "30%"
-vim.g.delve_use_vimux = 1
-
 require("lazy").setup({
   spec = {
     { "laktak/tome" },
-    { "christoomey/vim-tmux-navigator" },
 
     {
       "kyoh86/vim-go-coverage",
@@ -46,9 +41,33 @@ require("lazy").setup({
       },
     },
 
+    -- {
+    --   'tanvirtin/vgit.nvim',
+    --   dependencies = {
+    --     'nvim-lua/plenary.nvim',
+    --     'nvim-tree/nvim-web-devicons',
+    --   },
+    --   event = 'VimEnter',
+    --   opts = {},
+    --   keys = {
+    --     { "]c",  "<cmd>VGit hunk_down<cr>" },
+    --     { "[c",  "<cmd>VGit hunk_up<cr>" },
+    --
+    --     { "ghs", "<cmd>VGit buffer_hunk_stage<cr>" },
+    --     { "ghu", "<cmd>VGit buffer_hunk_reset<cr>" },
+    --     { "ghp", "<cmd>VGit buffer_hunk_preview<cr>" },
+    --
+    --     -- { "<leader>gb", "<cmd>VGit buffer_blame_preview<cr>" },
+    --     -- { "<leader>gl", "<cmd>VGit buffer_history_preview<cr>" },
+    --     -- { "<leader>gL", "<cmd>VGit project_logs_preview<cr>" },
+    --
+    --     -- { "<leader>gd", "<cmd>VGit buffer_diff_preview<cr>" },
+    --     -- { "<leader>gD", "<cmd>VGit project_diff_preview<cr>" },
+    --   },
+    -- },
+
     {
       "vim-test/vim-test",
-      dependencies = { "preservim/vimux" },
       keys = {
         { "<leader>tr", "<cmd>TestNearest<cr>" },
         { "<leader>tt", "<cmd>TestFile<cr>" },
@@ -57,12 +76,12 @@ require("lazy").setup({
 
     {
       "chrisgrieser/nvim-origami",
-      opts = { foldKeymaps = { setup = false } },
+      opts = { foldKeymaps = { setup = false }, autoFold = { enabled = false } },
     },
 
     {
       'stevearc/oil.nvim',
-      dependencies = { "nvim-tree/nvim-web-devicons" },
+      dependencies = { "nvim-tree/nvim-web-devicons", "benomahony/oil-git.nvim" },
       opts = { view_options = { show_hidden = true } },
       keys = { { "-", "<cmd>Oil<cr>" } },
       lazy = false,
@@ -75,7 +94,7 @@ require("lazy").setup({
 
     {
       "SCJangra/table-nvim",
-      opts = {},
+      opts = { next = 'right', prev = 'left' },
     },
 
     {
@@ -108,10 +127,8 @@ require("lazy").setup({
 
     {
       "sebdah/vim-delve",
-      lazy = false,
-      dependencies = { "preservim/vimux" },
       keys = {
-        { "<leader>dt", "<cmd>DlvTestCurrent<cr>" },
+        { "<leader>td", "<cmd>DlvTestCurrent<cr>" },
         { "<leader>db", "<cmd>DlvToggleBreakpoint<cr>" },
         { "<leader>dc", "<cmd>DlvConnect :2345<cr>" },
       },
@@ -132,29 +149,34 @@ require("lazy").setup({
     {
       "romgrk/barbar.nvim",
       lazy = false,
-      dependencies = {
-        'lewis6991/gitsigns.nvim',
-        'nvim-tree/nvim-web-devicons',
-      },
+      dependencies = { 'lewis6991/gitsigns.nvim', 'nvim-tree/nvim-web-devicons' },
       opts = {
         animation = false,
         icons = {
           button = "",
           modified = { button = "🔸" },
           pinned = { button = "📌", filename = true },
-          diagnostics = { [vim.diagnostic.severity.ERROR] = { enabled = true, icon = "🔥" }, }
+          diagnostics = { [vim.diagnostic.severity.ERROR] = { enabled = true, icon = "🔥" }, },
         },
       },
       keys = {
-        { "<s-h>", "<cmd>BufferPrevious<cr>" },
-        { "<s-l>", "<cmd>BufferNext<cr>" },
+        { "<s-h>",   "<cmd>BufferPrevious<cr>" },
+        { "<a-s-h>", "<cmd>BufferMovePrevious<cr>" },
+
+        { "<s-l>",   "<cmd>BufferNext<cr>" },
+        { "<a-s-l>", "<cmd>BufferMoveNext<cr>" },
+
+        { "<a-q>",   "<cmd>BufferClose<cr>" },
+        { "<a-s-q>", "<cmd>BufferCloseAllButCurrent<cr>" },
       }
     },
 
     {
       "ibhagwan/fzf-lua",
       lazy = false,
-      dependencies = { "nvim-tree/nvim-web-devicons" },
+      dependencies = {
+        "nvim-tree/nvim-web-devicons",
+      },
       opts = {
         winopts = {
           fullscreen = true,
@@ -176,13 +198,25 @@ require("lazy").setup({
 
     {
       "saghen/blink.cmp",
+      dependencies = { "xzbdmw/colorful-menu.nvim" },
       opts = {
         keymap = { preset = "enter" },
         signature = { enabled = true },
         appearance = { nerd_font_variant = "normal" },
         completion = {
           accept = { auto_brackets = { enabled = true } },
-          menu = { draw = { treesitter = { "lsp" } } },
+          -- menu = { draw = { treesitter = { "lsp" } } },
+          menu = {
+            draw = {
+              columns = { { "kind_icon" }, { "label", gap = 1 } },
+              components = {
+                label = {
+                  text = function(ctx) return require("colorful-menu").blink_components_text(ctx) end,
+                  highlight = function(ctx) return require("colorful-menu").blink_components_highlight(ctx) end,
+                },
+              },
+            },
+          },
           documentation = { auto_show = true, auto_show_delay_ms = 200 },
         },
         fuzzy = { prebuilt_binaries = { force_version = "v1.6.0" } },
@@ -215,11 +249,41 @@ require("lazy").setup({
           sources = {
             nls.builtins.code_actions.gomodifytags,
             nls.builtins.code_actions.impl,
-            nls.builtins.formatting.gofumpt,
-            nls.builtins.formatting.goimports,
+            -- nls.builtins.formatting.gofumpt,
+            -- nls.builtins.formatting.goimports,
           },
         })
       end,
+    },
+
+    {
+      'stevearc/conform.nvim',
+      opts = {
+        format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+        formatters_by_ft = { go = { "golangci", "gofmt" } },
+        -- formatters = {
+        --   golangci = {
+        --     command = "golangci-lint",
+        --     args = { "run", "--out-format", "json", "--fix", "$FILENAME" },
+        --   },
+        -- },
+        formatters = {
+          golangci = {
+            command = "golangci-lint",
+            args = {
+              "run",
+              -- "--out-format=json",
+              '--output.json.path=stdout',
+              "--issues-exit-code=0",
+              "--show-stats=false",
+              "--fix",
+              "$FILENAME",
+            },
+            -- cwd = require("conform.util").root_file({ ".golangci.yaml" }),
+          },
+        },
+
+      },
     },
 
     {
@@ -231,12 +295,12 @@ require("lazy").setup({
           local gitsigns = require("gitsigns")
           local opts = { buffer = bufnr }
 
-          vim.keymap.set("n", "]c", function()
-            if vim.wo.diff then vim.cmd.normal({ "]c", bang = true }) else gitsigns.nav_hunk("next") end
-          end, opts)
-          vim.keymap.set("n", "[c", function()
-            if vim.wo.diff then vim.cmd.normal({ "[c", bang = true }) else gitsigns.nav_hunk("prev") end
-          end, opts)
+          vim.keymap.set("n", "]c",
+            function() if vim.wo.diff then vim.cmd.normal({ "]c", bang = true }) else gitsigns.nav_hunk("next") end end,
+            opts)
+          vim.keymap.set("n", "[c",
+            function() if vim.wo.diff then vim.cmd.normal({ "[c", bang = true }) else gitsigns.nav_hunk("prev") end end,
+            opts)
 
           vim.keymap.set("n", "ghs", gitsigns.stage_hunk, opts)
           vim.keymap.set("n", "ghu", gitsigns.reset_hunk, opts)
@@ -252,25 +316,70 @@ require("lazy").setup({
         end
       }
     },
+
+    {
+      "greggh/claude-code.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+      config = function()
+        require("claude-code").setup()
+      end
+    },
+
+    {
+      "ldelossa/gh.nvim",
+      dependencies = {
+        {
+          "ldelossa/litee.nvim",
+          config = function()
+            require("litee.lib").setup()
+          end,
+        },
+      },
+      config = function()
+        require("litee.gh").setup()
+      end,
+    }
+
   },
-  checker = { enabled = true },
+  checker = { enabled = false },
 })
 
 
+vim.o.termguicolors = true
 vim.cmd.colorscheme("retrobox")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = nil })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = nil })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1C1C1C" })
+vim.api.nvim_set_hl(0, "Visual", { bg = "#252525" })
 vim.api.nvim_set_hl(0, "SignColumn", { bg = nil })
-vim.api.nvim_set_hl(0, "Visual", { fg = nil, bg = "#1C1C1C" })
+
 vim.api.nvim_set_hl(0, "Pmenu", { bg = "#1C1C1C" })
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#303030" })
+
 vim.api.nvim_set_hl(0, "PmenuKind", { fg = "#83A598", bg = "#1C1C1C" })
-vim.api.nvim_set_hl(0, "PmenuKindSel", { fg = "#1C1C1C", bg = "#83A598" })
+vim.api.nvim_set_hl(0, "PmenuKindSel", { bg = "#303030" })
+
 vim.api.nvim_set_hl(0, "PmenuExtra", { bg = "#1C1C1C" })
+vim.api.nvim_set_hl(0, "PmenuExtraSel", { bg = "#303030" })
+
+vim.api.nvim_set_hl(0, "PmenuMatch", { fg = "#B16286", bg = "#1C1C1C" })
+vim.api.nvim_set_hl(0, "PmenuMatchSel", { bg = "#303030" })
 
 vim.api.nvim_set_hl(0, "Added", { fg = "#B8BB26" })
 vim.api.nvim_set_hl(0, "Changed", { fg = "#83A598" })
 vim.api.nvim_set_hl(0, "Removed", { fg = "#FB4934" })
+
+vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#282808" })
+vim.api.nvim_set_hl(0, "DiffChange", { bg = "#19231f" })
+vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#420801" })
+vim.api.nvim_set_hl(0, "DiffText", { bg = "#522500" })
+
+vim.api.nvim_set_hl(0, "OilGitAdded", { fg = "#B8BB26" })
+vim.api.nvim_set_hl(0, "OilGitModified", { fg = "#83A598" })
+vim.api.nvim_set_hl(0, "OilGitRenamed", { fg = "#522500" })
+vim.api.nvim_set_hl(0, "OilGitUntracked", { fg = "#FB4934" })
 
 vim.api.nvim_set_hl(0, "Identifier", { fg = "#EBDBB2" })
 vim.api.nvim_set_hl(0, "Delimiter", { fg = "#EBDBB2" })
@@ -283,8 +392,11 @@ vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = "#EBDBB2" })
 vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = "#EBDBB2" })
 vim.api.nvim_set_hl(0, "DiagnosticOk", { fg = "#B8BB26" })
 
-require('fzf-lua').setup({ winopts = { fullscreen = true, preview = { layout = "vertical", vertical = "up:55%", border = "single" } } })
 require("fzf-lua").register_ui_select()
+
+vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+vim.opt.completeopt = "menuone,noselect,noinsert,fuzzy,popup"
 
 vim.opt.undodir = os.getenv("HOME") .. "/.local/nvim/undo"
 vim.opt.undofile = true
@@ -305,8 +417,9 @@ vim.opt.foldlevelstart = 99
 
 vim.opt.signcolumn = "auto"
 vim.opt.laststatus = 1
+vim.opt.cmdheight = 1
 
-vim.opt.updatetime = 100
+vim.opt.updatetime = 500
 
 if vim.fn.executable("ugrep") == 1 then
   vim.opt.grepprg = 'ugrep -nk --tabs=1 --ignore-files --exclude="zz_generated*" --exclude-dir="generated"'
@@ -314,12 +427,23 @@ else
   vim.opt.grepprg = 'grep -RIn --exclude="zz_generated*" --exclude-dir="generated"'
 end
 
+if vim.fn.has('nvim-0.12') == 1 then
+  vim.o.diffopt = 'internal,filler,closeoff,inline:word,linematch:40'
+elseif vim.fn.has('nvim-0.11') == 1 then
+  vim.o.diffopt = 'internal,filler,closeoff,linematch:40'
+end
+
 vim.cmd.packadd("cfilter")
 
-vim.filetype.add({ extension = { yaml = "helm" } })
+vim.filetype.add({
+  extension = {
+    yaml = "helm",
+    tpl = "helm",
+  }
+})
 
 vim.lsp.enable("gopls")
--- vim.lsp.enable("golangci_lint_ls")
+vim.lsp.enable("golangci_lint_ls")
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("helm_ls")
 
@@ -356,16 +480,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local client = vim.lsp.get_client_by_id(event.data.client_id)
     if client == nil then return end
 
-    if not client:supports_method("textDocument/willSaveWaitUntil")
-        and client:supports_method("textDocument/formatting") then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
-        buffer = event.buf,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = event.buf, id = client.id, timeout_ms = 1000 })
-        end,
-      })
-    end
+    -- if not client:supports_method("textDocument/willSaveWaitUntil")
+    --     and client:supports_method("textDocument/formatting") then
+    --   vim.api.nvim_create_autocmd("BufWritePre", {
+    --     group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
+    --     buffer = event.buf,
+    --     callback = function()
+    --      vim.lsp.buf.format({ bufnr = event.buf, id = client.id, timeout_ms = 1000 })
+    --     end,
+    --   })
+    -- end
 
     if client.server_capabilities.codeLensProvider then
       ---@diagnostic disable-next-line: param-type-mismatch
@@ -374,9 +498,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
       vim.keymap.set("n", "grc", "<cmd>lua vim.lsp.codelens.run()<cr>", opts)
     end
 
-    if client:supports_method("textDocument/formatting") then
-      vim.keymap.set({ "n", "x" }, "grf", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-    end
+    -- if client:supports_method("textDocument/formatting") then
+    --   vim.keymap.set({ "n", "x" }, "grf", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+    -- end
   end,
 })
 
@@ -390,7 +514,10 @@ local toggle_quickfix = function()
 end
 vim.keymap.set("n", "<leader>q", toggle_quickfix)
 
-vim.keymap.set("n", "<a-q>", "<cmd>bd<cr>")
+vim.keymap.set("n", "<leader>gg", "<cmd>tabnew term://lazygit<cr>i")
+
+vim.keymap.set("n", "<c-k>", "<cmd>cprev<cr>")
+vim.keymap.set("n", "<c-j>", "<cmd>cnext<cr>")
 
 vim.keymap.set("n", "<c-[>", "<cmd>noh<Return><esc>")
 
