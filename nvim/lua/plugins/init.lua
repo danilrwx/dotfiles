78248ddbca11fc6b604104926a43d4888b0f3fce
Daniil Antoshin <antoshindanil@ya.ru>
2026-03-09 16:48:59 +0200

    fix

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,76 +1,76 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(Plug)
+return require("packer").startup(function(Plug)
   -- Packer
-  Plug 'wbthomason/packer.nvim'
+  Plug "wbthomason/packer.nvim"
 
   -- Стартовый экран
-  Plug 'mhinz/vim-startify'
+  Plug "mhinz/vim-startify"
 
   -- Тема
-  Plug 'marko-cerovac/material.nvim'
+  Plug "marko-cerovac/material.nvim"
 
   -- Нижняя панель
   Plug {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true }
   }
 
   -- Добавляет end в конец блока
-  Plug 'tpope/vim-endwise'
+  Plug "tpope/vim-endwise"
 
   -- Переключение раскладки в insert
-  Plug 'rlue/vim-barbaric'
+  Plug "rlue/vim-barbaric"
 
   -- Файловый менеджер
-  Plug 'kevinhwang91/rnvimr'
+  Plug "kevinhwang91/rnvimr"
 
   -- Великий гит
-  Plug 'kdheepak/lazygit.nvim'
-  Plug 'tpope/vim-fugitive'
-  Plug 'lewis6991/gitsigns.nvim'
+  Plug "kdheepak/lazygit.nvim"
+  Plug "tpope/vim-fugitive"
+  Plug "lewis6991/gitsigns.nvim"
 
   -- LSP
-  Plug 'williamboman/nvim-lsp-installer'
-  Plug { 'ms-jpq/coq_nvim', run = 'python3 -m coq deps' }
-  Plug 'ms-jpq/coq.artifacts'
-  Plug 'ms-jpq/coq.thirdparty'
-  Plug 'neovim/nvim-lspconfig'
-  Plug 'kosayoda/nvim-lightbulb'
+  Plug "williamboman/nvim-lsp-installer"
+  Plug { "ms-jpq/coq_nvim", run = "python3 -m coq deps" }
+  Plug "ms-jpq/coq.artifacts"
+  Plug "ms-jpq/coq.thirdparty"
+  Plug "neovim/nvim-lspconfig"
+  Plug "kosayoda/nvim-lightbulb"
   Plug "b0o/schemastore.nvim"
 
   -- Подсветка синтаксиса
-  Plug { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  Plug 'nvim-treesitter/nvim-treesitter-textobjects'
-  Plug 'windwp/nvim-ts-autotag'
-  Plug 'andymass/vim-matchup'
-  Plug 'JoosepAlviste/nvim-ts-context-commentstring'
+  Plug { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+  Plug "nvim-treesitter/nvim-treesitter-textobjects"
+  Plug "windwp/nvim-ts-autotag"
+  Plug "andymass/vim-matchup"
+  Plug "JoosepAlviste/nvim-ts-context-commentstring"
 
   -- Для руби
-  Plug 'tpope/vim-rails'
-  Plug 'vim-ruby/vim-ruby'
-  Plug 'tpope/vim-bundler'
-  Plug 'slim-template/vim-slim'
+  Plug "tpope/vim-rails"
+  Plug "vim-ruby/vim-ruby"
+  Plug "tpope/vim-bundler"
+  Plug "slim-template/vim-slim"
 
   -- Парные теги
-  Plug 'windwp/nvim-autopairs'
+  Plug "windwp/nvim-autopairs"
 
   -- Подсветка парных тегов
-  Plug 'Valloric/MatchTagAlways'
+  Plug "Valloric/MatchTagAlways"
 
   -- Плавный скролл
-  Plug 'karb94/neoscroll.nvim'
+  Plug "karb94/neoscroll.nvim"
 
   -- Подсветка отступов
-  Plug 'nathanaelkane/vim-indent-guides'
+  Plug "nathanaelkane/vim-indent-guides"
 
   -- Зависимости для popup плагинов
-  Plug 'nvim-lua/plenary.nvim'
-  Plug 'nvim-lua/popup.nvim'
+  Plug "nvim-lua/plenary.nvim"
+  Plug "nvim-lua/popup.nvim"
 
   -- Поиск
-  Plug 'nvim-telescope/telescope.nvim'
+  Plug "nvim-telescope/telescope.nvim"
 
   -- Найти и заменить
-  Plug 'windwp/nvim-spectre'
+  Plug "windwp/nvim-spectre"
 end)
