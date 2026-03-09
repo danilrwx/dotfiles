cecc656537aa6161174a3431da7833d6bdaba6db
Daniil Antoshin <antoshindanil@ya.ru>
2026-03-09 16:55:16 +0200

    +
    Signed-off-by: Daniil Antoshin <antoshindanil@ya.ru>

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -35,6 +35,7 @@ return {
     config = function(_, opts)
       local lspconfig = require('lspconfig')
       for server, config in pairs(opts.servers) do
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
         lspconfig[server].setup(config)
       end
     end
