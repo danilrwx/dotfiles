96fb26d1219b445e893f6ea754c2e37bdcfd3fac
Daniil Antoshin <antoshindanil@ya.ru>
2026-03-09 16:53:31 +0200

    +
    Signed-off-by: Daniil Antoshin <antoshindanil@ya.ru>

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -3,31 +3,12 @@ vim.keymap.set("n", "<A-q>", "<cmd>bd<CR>", { desc = "Close buffer by A-q" })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center view after paging" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center view after paging" })
---
-vim.keymap.del("n", "<leader>gg")
-vim.keymap.del("n", "<leader>gG")
-vim.keymap.del("n", "<leader>gf")
-vim.keymap.del("n", "<leader>gB")
-vim.keymap.del("n", "<leader><leader>")
-
-vim.keymap.set("n", "<leader>gg", "<cmd>!topen-git<Return><Esc>", { desc = "Open LazyGit" })
-
-vim.keymap.set("n", "<leader>g=", "<cmd>tab Git<CR>", { desc = "Fugitive" })
---
-vim.keymap.set("n", "<leader>gl", "<cmd>tab Git log -p --follow %<CR>", { desc = "Git detailed file log" })
-vim.keymap.set("n", "<leader>gL", "<cmd>tab Git log<CR>", { desc = "Git log" })
-
-vim.keymap.set("n", "<leader>gb", "<cmd>tab Git blame<CR>", { desc = "Git blame" })
-
-vim.keymap.set("n", "<leader>go", "<cmd>GBrowse<CR>", { desc = "Git browse" })
-
-vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit<CR>", { desc = "Git diff file" })
-vim.keymap.set("n", "<leader>gD", "<cmd>Gvdiffsplit HEAD<CR>", { desc = "Git diff with staged" })
 
 vim.keymap.set("n", "<Leader>su", vim.cmd.UndotreeToggle, { desc = "Open UndoTree" })
 
+vim.keymap.del("n", "<leader>e")
 local MiniFiles = require("mini.files")
-vim.keymap.set("n", "<C-f>", function()
+vim.keymap.set("n", "<leader>e", function()
   if not MiniFiles.close() then
     MiniFiles.open(vim.api.nvim_buf_get_name(0), true)
   end
