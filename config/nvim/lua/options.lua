cfbc6ac4f7aa7a0892e5cd09a8b35b08e0cd4bda
Daniil Antoshin <antoshindanil@ya.ru>
2026-03-09 16:49:51 +0200

    feat: minimize

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,11 +1,11 @@
 local langmap_keys     = {
-  'ёЁ;`~', '№;#',
-  'йЙ;qQ', 'цЦ;wW', 'уУ;eE', 'кК;rR', 'еЕ;tT', 'нН;yY', 'гГ;uU', 'шШ;iI', 'щЩ;oO', 'зЗ;pP', 'хХ;[{', 'ъЪ;]}',
-  'фФ;aA', 'ыЫ;sS', 'вВ;dD', 'аА;fF', 'пП;gG', 'рР;hH', 'оО;jJ', 'лЛ;kK', 'дД;lL', [[жЖ;\;:]], [[эЭ;'\"]],
-  'яЯ;zZ', 'чЧ;xX', 'сС;cC', 'мМ;vV', 'иИ;bB', 'тТ;nN', 'ьЬ;mM', [[бБ;\,<]], 'юЮ;.>',
+  "ёЁ;`~", "№;#",
+  "йЙ;qQ", "цЦ;wW", "уУ;eE", "кК;rR", "еЕ;tT", "нН;yY", "гГ;uU", "шШ;iI", "щЩ;oO", "зЗ;pP", "хХ;[{", "ъЪ;]}",
+  "фФ;aA", "ыЫ;sS", "вВ;dD", "аА;fF", "пП;gG", "рР;hH", "оО;jJ", "лЛ;kK", "дД;lL", [[жЖ;\;:]], [[эЭ;"\"]],
+  "яЯ;zZ", "чЧ;xX", "сС;cC", "мМ;vV", "иИ;bB", "тТ;nN", "ьЬ;mM", [[бБ;\,<]], "юЮ;.>",
 }
 
-vim.o.langmap          = table.concat(langmap_keys, ',')
+vim.o.langmap          = table.concat(langmap_keys, ",")
 vim.g.mapleader        = " "
 vim.opt.undodir        = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile       = true
@@ -19,9 +19,9 @@ vim.opt.softtabstop    = 2
 vim.opt.tabstop        = 2
 vim.opt.shiftwidth     = 2
 vim.opt.colorcolumn    = { 120 }
-vim.opt.signcolumn     = "number"
+-- vim.opt.signcolumn     = "number"
 
--- vim.opt.listchars      = { space = ' ', tab = '│ ' }
+-- vim.opt.listchars      = { space = " ", tab = "│ " }
 -- vim.opt.list           = true
 
 
