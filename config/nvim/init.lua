ab7c0aef4443b5b7d4a4788de1634d01a442f445
Daniil Antoshin <antoshindanil@ya.ru>
2026-03-09 16:58:06 +0200

    update
    Signed-off-by: Daniil Antoshin <antoshindanil@ya.ru>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -35,6 +35,9 @@ require("lazy").setup({
       "catppuccin/nvim",
       name = "catppuccin",
       priority = 1000,
+      opts = {
+        transparent_background = true,
+      },
     },
 
     {
