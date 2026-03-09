27d04c19b17f2589217924eaaedd5783c5013d3e
Daniil Antoshin <antoshindanil@ya.ru>
2026-03-09 16:49:09 +0200

    fix git

--- .config/nvim/after/plugin/git.lua
+++ .config/nvim/after/plugin/git.lua
@@ -1,1 +1,5 @@
 vim.keymap.set("n", "<Leader>gg", ":LazyGit<CR>")
+
+require('gitsigns').setup({
+  current_line_blame = true
+})
--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -12,22 +12,3 @@ vim.keymap.set("n", "<Leader>S", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 vim.keymap.set("n", "<Esc>", ":noh<Return><Esc>")
 
-vim.keymap.set("n", "<Leader>co", ":copen<CR>")
-vim.keymap.set("n", "<Leader>cc", ":cclose<CR>")
-
-vim.cmd([[
-  function! QuickfixMapping()
-    nnoremap <buffer> K :cprev<CR>zz<C-w>w
-    nnoremap <buffer> J :cnext<CR>zz<C-w>w
-    nnoremap <buffer> <leader>u :set modifiable<CR>
-    nnoremap <buffer> <leader>w :cgetbuffer<CR>:cclose<CR>:copen<CR>
-    nnoremap <buffer> <leader>r :cdo s/// \| update<C-Left><C-Left><Left><Left><Left>
-  endfunction
-
-  augroup quickfix_group
-    autocmd!
-    autocmd filetype qf call QuickfixMapping()
-    autocmd filetype qf setlocal errorformat+=%f\|%l\ col\ %c\|%m
-  augroup END
-]])
-
