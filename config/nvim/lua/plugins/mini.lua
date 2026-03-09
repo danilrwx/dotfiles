2cce141a1cb41fe24e538dcda03d1ae0d84da923
Daniil Antoshin <antoshindanil@ya.ru>
2026-03-09 16:49:51 +0200

    feat: return old nvim config

---
+++ config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+    config = function()
+      require("mini.bracketed").setup()
+      require("mini.diff").setup()
+      require("mini.notify").setup()
+      require("mini.statusline").setup()
+      require("mini.surround").setup()
+      require("mini.trailspace").setup()
+      require('mini.icons').setup()
+
+      local MiniAi = require('mini.ai')
+      MiniAi.setup({
+        search_method = 'cover_or_nearest',
+        custom_textobjects = {
+          f = MiniAi.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
+          a = MiniAi.gen_spec.treesitter({ a = '@parameter.outer', i = '@parameter.inner' }),
+          i = MiniAi.gen_spec.treesitter({ a = '@conditional.outer', i = '@conditional.inner' }),
+          l = MiniAi.gen_spec.treesitter({ a = '@loop.outer', i = '@loop.inner' }),
+        }
+      })
+
+      local MiniFiles = require("mini.files")
+      MiniFiles.setup()
+      vim.keymap.set("n", "<Leader><Leader>",
+        function() if not MiniFiles.close() then MiniFiles.open(vim.api.nvim_buf_get_name(0), true) end end, {})
+    end
+  }
+}
