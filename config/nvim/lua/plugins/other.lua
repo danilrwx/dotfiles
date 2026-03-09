9fc15d333af11ffc93a7e5bc7bf66efd2212666f
Daniil Antoshin <antoshindanil@ya.ru>
2026-03-09 16:54:17 +0200

    +
    Signed-off-by: Daniil Antoshin <antoshindanil@ya.ru>

--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -17,29 +17,4 @@ return {
     keys = { { "-", "<cmd>Oil<cr>" } },
     lazy = false,
   },
-  {
-    'romgrk/barbar.nvim',
-    lazy = false,
-    dependencies = {
-      'lewis6991/gitsigns.nvim',
-      'nvim-tree/nvim-web-devicons',
-    },
-    keys = {
-      { "<s-h>", "<cmd>BufferPrevious<cr>" },
-      { "<s-l>", "<cmd>BufferNext<cr>" },
-    },
-    init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
-      animation = false,
-      icons = {
-        button = '',
-        modified = { button = '🔸' },
-        pinned = { button = '📌', filename = true },
-        diagnostics = {
-          [vim.diagnostic.severity.ERROR] = { enabled = true, icon = '🔥' },
-        }
-      },
-    },
-    version = '^1.0.0',
-  },
 }
