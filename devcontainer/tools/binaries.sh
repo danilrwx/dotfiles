60df04b16f567a005f6fbc001e3945713e50aa64
Daniil Antoshin <antoshindanil@ya.ru>
2026-07-05 15:25:46 +0200

    feat(devbox): install glab in the container
    Signed-off-by: Daniil Antoshin <antoshindanil@ya.ru>

--- devcontainer/tools/binaries.sh
+++ devcontainer/tools/binaries.sh
@@ -35,6 +35,11 @@ gh_ver="$(latest_tag cli/cli)"
 curl -fsSL "https://github.com/cli/cli/releases/download/${gh_ver}/gh_${gh_ver#v}_linux_${ARCH}.tar.gz" | tar -xzf - -C "$tmp"
 $SUDO install "$tmp/gh_${gh_ver#v}_linux_${ARCH}/bin/gh" /usr/local/bin/gh
 
+# glab (gitlab release; asset unpacks into bin/glab)
+glab_ver="$(basename "$(curl -fsSLo /dev/null -w '%{url_effective}' https://gitlab.com/gitlab-org/cli/-/releases/permalink/latest)")"
+curl -fsSL "https://gitlab.com/gitlab-org/cli/-/releases/${glab_ver}/downloads/glab_${glab_ver#v}_linux_${ARCH}.tar.gz" | tar -xzf - -C "$tmp"
+$SUDO install "$tmp/bin/glab" /usr/local/bin/glab
+
 # werf
 werf_ver="$(curl -fsSL "https://tuf.werf.io/targets/channels/${WERF_CHANNEL}")"
 curl -fsSL "https://tuf.werf.io/targets/releases/${werf_ver}/linux-${ARCH}/bin/werf" | $SUDO tee /usr/local/bin/werf >/dev/null
